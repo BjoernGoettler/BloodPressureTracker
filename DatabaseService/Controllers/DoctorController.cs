a3d2f56f8a040b628159a99f1b3cd7b30d7144fc
Bjørn Göttler <bjoern@goettler.dk>
2024-11-06 22:45:56 +0100

    feature: database service: Controllers

---
+++ DatabaseService/Controllers/DoctorController.cs
@@ -0,0 +1,108 @@
+using DatabaseService.DataInOutObjects;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DatabaseService.Models;
+
+namespace DatabaseService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class DoctorController : ControllerBase
+    {
+        private readonly MeasurementsContext _context;
+
+        public DoctorController(MeasurementsContext context)
+        {
+            _context = context;
+        }
+
+        // GET: api/Doctor
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<DoctorModel>>> GetDoctors()
+        {
+            return await _context.Doctors.ToListAsync();
+        }
+
+        // GET: api/Doctor/5
+        [HttpGet("{id}")]
+        public async Task<ActionResult<DoctorModel>> GetDoctorModel(int id)
+        {
+            var doctorModel = await _context.Doctors.FindAsync(id);
+
+            if (doctorModel == null)
+            {
+                return NotFound();
+            }
+
+            return doctorModel;
+        }
+
+        // PUT: api/Doctor/5
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPut("{id}")]
+        public async Task<IActionResult> PutDoctorModel(int id, DoctorModel doctorModel)
+        {
+            if (id != doctorModel.DoctorId)
+            {
+                return BadRequest();
+            }
+
+            _context.Entry(doctorModel).State = EntityState.Modified;
+
+            try
+            {
+                await _context.SaveChangesAsync();
+            }
+            catch (DbUpdateConcurrencyException)
+            {
+                if (!DoctorModelExists(id))
+                {
+                    return NotFound();
+                }
+                else
+                {
+                    throw;
+                }
+            }
+
+            return NoContent();
+        }
+
+        // POST: api/Doctor
+        // To protect from overposting attacks, see https://go.microsoft.com/fwlink/?linkid=2123754
+        [HttpPost]
+        public async Task<ActionResult<DoctorModel>> PostDoctorModel(DoctorIn doctorIn)
+        {
+            var doctorModel = new DoctorModel
+            {
+                Name = doctorIn.Name,
+            };
+            
+            _context.Doctors.Add(doctorModel);
+            await _context.SaveChangesAsync();
+
+            return CreatedAtAction("GetDoctorModel", new { id = doctorModel.DoctorId }, doctorModel);
+        }
+
+        // DELETE: api/Doctor/5
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> DeleteDoctorModel(int id)
+        {
+            var doctorModel = await _context.Doctors.FindAsync(id);
+            if (doctorModel == null)
+            {
+                return NotFound();
+            }
+
+            _context.Doctors.Remove(doctorModel);
+            await _context.SaveChangesAsync();
+
+            return NoContent();
+        }
+
+        private bool DoctorModelExists(int id)
+        {
+            return _context.Doctors.Any(e => e.DoctorId == id);
+        }
+    }
+}
--- DatabaseService/Controllers/PatientController.cs
+++ DatabaseService/Controllers/PatientController.cs
@@ -1,6 +1,82 @@
-namespace DatabaseService.Controllers;
+using System.Collections.ObjectModel;
+using System.Diagnostics;
+using DatabaseService.DataInOutObjects;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using DatabaseService.Models;
 
-public class PatientController
+namespace DatabaseService.Controllers
 {
+    [Route("api/[controller]")]
+    [ApiController]
+    public class PatientController: ControllerBase
+    {
+        private readonly MeasurementsContext _context;
+
+        public PatientController(MeasurementsContext context)
+        {
+            _context = context;
+        }
+
+        [HttpPost]
+        public async Task<ActionResult<PatientModel>> Post(PatientIn patientIn)
+        {
+            var patientModel = new PatientModel
+            {
+                Ssn = patientIn.SSN,
+                Name = patientIn.Name,
+                Email = patientIn.Email,
+            };
+            
+            await _context.Patients.AddAsync(patientModel);
+            await _context.SaveChangesAsync();
+            
+            return patientModel;
+        }
+
+        [HttpPost("[action]")]
+        public async Task<ActionResult<PatientMeasurementsOut>> Post(MeasurementIn measurementIn)
+        {
+            var patient = await _context.Patients.FirstOrDefaultAsync(x => x.Ssn == measurementIn.Ssn);
+            if (patient == null)
+            {
+                return NotFound("Patient not found");
+            }
+
+            patient.Measurements ??= new List<MeasurementsModel>();
+          
+            var measurement = new MeasurementsModel{
+                Date = DateTime.Now,
+                Systolic = measurementIn.Systolic,
+                Diastolic = measurementIn.Diastolic,
+                Patient = patient
+            };
+            
+            patient.Measurements.Add(measurement);
+            
+            //_context.Patients.Update(patient);
+            await _context.SaveChangesAsync();
+            _context.Patients.Update(patient);
+            
+            var patientMeasurementOut = new PatientMeasurementsOut
+            {
+                Ssn = patient.Ssn,
+                Measurements = new List<MeasurementOut>()
+            };
+            foreach (var existingMeasurements in patient.Measurements)
+            {
+                var measurementOut = new MeasurementOut
+                {
+                    Date = measurement.Date,
+                    Systolic = measurementIn.Systolic,
+                    Diastolic = measurement.Diastolic,
+                    Seen = (existingMeasurements.Seen == null) ? false : true
+                };
+                patientMeasurementOut.Measurements.Add(measurementOut);
+            }
+            return patientMeasurementOut;
+        }
     
+    }
 }
+
